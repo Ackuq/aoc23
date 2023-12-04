fd45d2d475e41d9f4f6264a20175722b08e585d1
Axel Pettersson <axel@pettersson.cc>
2023-12-04 07:22:39 +0100

    Refactor code to use a helper function for getting the winning hand

--- aoc23/day4/solution.py
+++ aoc23/day4/solution.py
@@ -1,26 +1,26 @@
 import re
-from typing import List
+from typing import List, Set
 
 regex = r"Card\s+\d+: (.*) \| (.*)"
+
+
+def get_winning_hand(row: str) -> Set[int]:
+    match = re.match(regex, row)
+    if match is None:
+        return set()
+    winners = set(
+        int(number) for number in match.group(1).strip().split(" ") if number != ""
+    )
+    hand = set(
+        int(number) for number in match.group(2).strip().split(" ") if number != ""
+    )
+    return hand.intersection(winners)
 
 
 def part1(input: List[str]) -> None:
     result = 0
     for row in input:
-        match = re.match(regex, row)
-
-        if match is None:
-            continue
-
-        winning = set(
-            int(number) for number in match.group(1).strip().split(" ") if number != ""
-        )
-        hand = set(
-            int(number) for number in match.group(2).strip().split(" ") if number != ""
-        )
-
-        winning_hand = hand.intersection(winning)
-
+        winning_hand = get_winning_hand(row)
         if len(winning_hand) > 0:
             result += 2 ** (len(winning_hand) - 1)
 
@@ -31,20 +31,8 @@ def part2(input: List[str]) -> None:
     scratch_boards = {index: 1 for index, _ in enumerate(input)}
 
     for index, row in enumerate(input):
-        match = re.match(regex, row)
-
-        if match is None:
-            continue
-
-        winning = set(
-            int(number) for number in match.group(1).strip().split(" ") if number != ""
-        )
-        hand = set(
-            int(number) for number in match.group(2).strip().split(" ") if number != ""
-        )
-
         multiply = scratch_boards[index]
-        winning_hand = hand.intersection(winning)
+        winning_hand = get_winning_hand(row)
 
         for number in range(index + 1, index + len(winning_hand) + 1):
             scratch_boards[number] += multiply
