2ced9f5f52d026d9faa26d51bde13f1dd6ee5003
Axel Pettersson <axel@pettersson.cc>
2023-12-13 22:21:18 +0100

    Finish day 12 part 2

--- aoc23/day12/solution.py
+++ aoc23/day12/solution.py
@@ -1,68 +1,75 @@
-import re
-from copy import deepcopy
+from functools import cache, reduce
 from typing import List, Tuple
 
-import tqdm
-
-Input = List[Tuple[List[str], List[int]]]
+Input = List[Tuple[Tuple[str, ...], Tuple[int, ...]]]
 
 
 def parse_input(lines: List[str]) -> Input:
     input = []
     for line in lines:
         conditions_str, sizes_str = line.strip().split(" ")
-        conditions = list(conditions_str)
-        sizes = [int(size) for size in sizes_str.split(",")]
+        conditions = tuple(conditions_str)
+        sizes = tuple(int(size) for size in sizes_str.split(","))
         input.append((conditions, sizes))
 
     return input
 
 
+@cache
+def is_valid(rest: Tuple[str], sizes: Tuple[int]) -> int:
+    # If we have no more conditions and no more sizes, we have a valid path
+    if (len(rest) == 0 and len(sizes) == 0) or (
+        # If we have no more sizes, we can only have dots and question marks
+        len(sizes) == 0
+        and all(c == "." or c == "?" for c in rest)
+    ):
+        return 1
+    # If we have no more conditions or no more sizes, we have an invalid path
+    if len(rest) == 0 or len(sizes) == 0:
+        return 0
+    current = rest[0]
+    if current == "#":
+        if (
+            # The rest of the path must be at least as long as the current size
+            len(rest) >= sizes[0]
+            # We need a consecutive sequence of broken springs
+            and all(c == "#" or c == "?" for c in rest[: sizes[0]])
+            and (
+                # If we have reached the end of the path, we have a valid path
+                # Otherwise the next spring must be functional
+                len(rest) == sizes[0]
+                or rest[sizes[0]] == "."
+                or rest[sizes[0]] == "?"
+            )
+        ):
+            return is_valid(rest[sizes[0] + 1 :], sizes[1:])
+        return 0
+    if current == ".":
+        return is_valid(rest[1:], sizes)
+    if current == "?":
+        # We can either have a broken spring or a functional spring
+        return is_valid(("#",) + rest[1:], sizes) + is_valid((".",) + rest[1:], sizes)
+
+    raise Exception("Invalid input")
+
+
 def part1(input: Input) -> None:
-    def generate_possibilities(conditions: List[str]) -> List[List[str]]:
-        possibilities: List[List[str]] = []
-        for condition in conditions:
-            if condition == "?":
-                left = deepcopy(possibilities) if len(possibilities) > 0 else [[]]
-                right = deepcopy(possibilities) if len(possibilities) > 0 else [[]]
-                for possibility in left:
-                    possibility.append("#")
-                for possibility in right:
-                    possibility.append(".")
-                possibilities = left + right
-            if condition == "#":
-                if len(possibilities) == 0:
-                    possibilities = [["#"]]
-                    continue
-                for possibility in possibilities:
-                    possibility.append("#")
-            if condition == ".":
-                if len(possibilities) == 0:
-                    possibilities = [["."]]
-                    continue
-                for possibility in possibilities:
-                    possibility.append(".")
-        return possibilities
-
-    def generate_regex(sizes: List[int]) -> str:
-        regex = r"^\.*"
-        for size in sizes[:-1]:
-            regex += f"#{{{size}}}" + r"\.+"
-        regex += f"#{{{sizes[-1]}}}" + r"\.*$"
-        return regex
-
     result = 0
-    for conditions, sizes in tqdm.tqdm(input):
-        regex = generate_regex(sizes)
-        possibilities = generate_possibilities(conditions)
-        for possibility in possibilities:
-            match = re.match(regex, "".join(possibility))
-            if match is not None:
-                result += 1
-
+    for conditions, sizes in input:
+        result += is_valid(conditions, sizes)
     print("Part 1:", result)
+
+
+def part2(input: Input) -> None:
+    result = 0
+    for conditions, sizes in input:
+        new_sizes = sizes * 5
+        new_conditions = reduce(lambda a, b: a + ("?",) + b, (conditions,) * 5)
+        result += is_valid(new_conditions, new_sizes)
+    print("Part 2:", result)
 
 
 def main(lines: List[str]) -> None:
     input = parse_input(lines)
     part1(input)
+    part2(input)
